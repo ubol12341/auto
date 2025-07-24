25b510ab7c2d8d931b727a8e150f74195e352c0e
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 21:46:20 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
+yes | pkg update && pkg upgrade
+yes | pkg install libjansson build-essential clang binutils git dialog
+yes | pkg install python3
+yes | pkg install libjansson wget nano
+
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
 
 # Clone repository 
