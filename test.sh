4c94b1baeea9439c99d848a598b054e91059a91d
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 21:28:43 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+yes | sudo apt-get update 
+yes | sudo apt-get upgrade 
+yes | sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev figlet
+yes | sudo apt-get install python3
+yes | sudo apt-get install libjansson wget nano
+yes | sudo apt-get install nano
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+
+
+
+
+# Clone repository 
+
+git clone https://github.com/nidkit1234/tr.git
+cd tr
+
+
+chmod +x trnet.sh 
+mv ./trnet.sh ~/
+
+echo 'cd tr && ./trnet.sh' >> ~/.bashrc
+
+
+
+
+  
+  
