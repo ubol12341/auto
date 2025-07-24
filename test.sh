d9286f2345364ba86f859c4f8845dcbe98ef0aca
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 21:42:19 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,3 @@
-yes | sudo apt-get update 
-yes | sudo apt-get upgrade 
-yes | sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev figlet
-yes | sudo apt-get install python3
-yes | sudo apt-get install libjansson wget nano
-yes | sudo apt-get install nano
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
-
-
 
 
 # Clone repository 
