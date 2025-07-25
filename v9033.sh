6cc3e718682a969b52778bfa3f50bbf0d79ef101
ubol12341 <ubol1234.1@gmail.com>
2025-07-25 12:08:19 +0700

    Add files via upload

---
+++ v9033.sh
@@ -0,0 +1,24 @@
+yes | pkg update && pkg upgrade
+yes | pkg install libjansson build-essential clang binutils git dialog
+yes | pkg install python3
+yes | pkg install libjansson wget nano
+
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+
+
+# Clone repository 
+
+git clone https://github.com/ubol12341/tr.git
+cd tr
+
+
+chmod +x trv9033.sh 
+mv ./trv9033.sh ~/
+
+echo './trv9033.sh' >> ~/.bashrc
+
+
+
+
+  
+  
